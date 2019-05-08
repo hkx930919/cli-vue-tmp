4acb2f99230fe5d2861836b9113c9f4ab0e08392
jin_gang <jing_gang@hunliji.com>
2019-05-08 19:58:56 +0800

    项目config.js

--- build/webpack.util.js
+++ build/webpack.util.js
@@ -1,24 +1,35 @@
 const path = require('path')
 const fs = require('fs')
 const { PAGES_DIR } = require('./webpack.const')
+const { entry } = require('../config')
 // const cwd = process.cwd()
+
 // 获取文件夹名称
 function getPages() {
-  return fs.readdirSync(PAGES_DIR).filter(dir => {
-    console.log('*******')
-    console.log(path.join(PAGES_DIR, dir, 'main.js'))
-    console.log('*******')
-    let filePath = path.join(PAGES_DIR, dir, 'main.js')
-    if (!fs.existsSync(filePath)) {
-      filePath = `${filePath}x`
+  let pages
+  if (entry) {
+    if (Array.isArray(entry)) {
+      pages = entry
+    } else {
+      pages = [entry]
     }
-    if (!fs.existsSync(filePath)) {
-      return false
-    }
-    return true
-  })
+  } else {
+    pages = fs.readdirSync(PAGES_DIR).filter(dir => {
+      console.log('*******')
+      console.log(path.join(PAGES_DIR, dir, 'main.js'))
+      console.log('*******')
+      let filePath = path.join(PAGES_DIR, dir, 'main.js')
+      if (!fs.existsSync(filePath)) {
+        filePath = `${filePath}x`
+      }
+      if (!fs.existsSync(filePath)) {
+        return false
+      }
+      return true
+    })
+  }
+  return pages
 }
-
 module.exports = {
   getPages
 }
---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * 项目配置页
+ */
+module.exports = {
+  /**entry 可以为string,数组,如果为false类型，那么读取项目目录
+   *   entry: 'example'
+   *   entry: ['example1','example2']
+   */
+  entry: 'example'
+}
